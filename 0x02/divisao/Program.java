160b3ef64f4a8e69204fed1a2008a22316047c4d
Thomaz França <thomazf@ciandt.com>
2022-06-14 14:39:52 -0300

    Exceções (Parte I)

---
+++ 0x02/divisao/Program.java
@@ -0,0 +1,15 @@
+public class Program {
+    public static void main(String[] args) {
+
+        int a, b;
+
+        a = 4;
+        b = 3;
+        UmNumero.dividir(a, b);
+        System.out.println();
+
+        a = 9;
+        b = 0;
+        UmNumero.dividir(a, b);
+    }
+}
---
+++ 0x02/divisao/UmNumero.java
@@ -0,0 +1,15 @@
+public class UmNumero {
+    public static void dividir(int a, int b) {
+        int result = (0);
+
+        try {
+            result = a/b;
+
+        } catch (Exception ex) {
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+            System.out.println(a + " / " + b + " = " + result);
+        }
+
+    }
+}
