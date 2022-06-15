bc2e2c256028fcb18a100cba736426e3b8b16afc
Thomaz França <thomazf@ciandt.com>
2022-06-15 15:20:04 -0300

    Streams, Collectors e Optional

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> listProduto = pedido.getProdutos();
+        return listProduto.stream().filter(p -> p.getCategoria() == CategoriaProduto.LIVRO).collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> listaProduto) {
+        Optional<Produto> result = listaProduto.stream().sorted(comparing(Produto::getPreco).reversed()).findFirst();
+        return result.orElse(null);
+    }
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> listaProduto, double preco) {
+        Stream<Produto> stream = listaProduto.stream().filter(i -> i.getPreco() >= preco);
+        return stream.collect(Collectors.toList());
+    }
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedido) {
+
+        return pedido.stream().filter(
+                        p -> p.getProdutos().stream().anyMatch(t -> t.getCategoria().equals(CategoriaProduto.ELETRONICO)))
+                .collect(Collectors.toList());
+    }
+
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream().map(produto -> {
+            if (produto.getCategoria().equals(CategoriaProduto.ELETRONICO))
+                produto.setPreco(produto.getPreco() - produto.getPreco() * 0.15);
+            return produto;
+        }).collect(Collectors.toList());
+    }
+}
