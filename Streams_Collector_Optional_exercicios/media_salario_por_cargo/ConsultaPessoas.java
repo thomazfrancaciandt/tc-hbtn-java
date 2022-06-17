2156d162c70aa0a77cab8a13f95d8ab488d9c780
Thomaz França <thomazf@ciandt.com>
2022-06-17 09:09:42 -0300

    Task 3

---
+++ Streams_Collector_Optional_exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(
+                Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+
+    }
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(
+            List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)));
+    }
+
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.filtering(i -> i.getIdade() >= 40, Collectors.toList())));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> todasPessoas) {
+
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+}
+
