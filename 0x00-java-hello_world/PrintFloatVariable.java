6b856c76e4cd01053dd9940aae9f2bbcc69a12de
Thomaz França <thomazf@ciandt.com>
2022-07-26 08:20:30 -0300

    Java – Criando seu primeiro programa

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable{
     public static void main(String[] args) {
-        float Pi = 3.142f; System.out.println ("Valor de pi: " + Pi);
+        float Pi = 3.142f;
+        System.out.println ("Valor de pi: " + Pi);
     }
-
 }
