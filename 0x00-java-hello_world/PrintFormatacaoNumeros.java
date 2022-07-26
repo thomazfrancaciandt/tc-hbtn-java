27bd9aa4a2e080bb5dbbb18c928b8f49b82e176a
Thomaz França <thomazf@ciandt.com>
2022-07-26 09:48:02 -0300

    Java – Criando seu primeiro programa

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,11 +1,8 @@
-import java.text.NumberFormat;
-import java.util.Locale;
-
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: R$ " + String.format("%.8s", NumberFormat.getInstance(Locale.US)
-                .format(valor)) + "\n" + "Taxa: %.2f%%\n", taxa);
+        System.out.printf("Valor: R$ %,1.2f\n", valor);
+        System.out.printf("Taxa: %.2f%%\n", taxa);
     }
 }
